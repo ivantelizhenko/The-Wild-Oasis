b40fb6c4ea15c68e84b91a4ee91012a93bcf742a
ivantelizhenko <telizenkoo@gmail.com>
2024-09-10 23:30:23 +0300

    upload data 1

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import Logo from './Logo';
 import MainNav from './MainNav';
+import Uploader from '../../src/data/Uploader';
 
 const StyledSidebar = styled.aside`
   display: flex;
